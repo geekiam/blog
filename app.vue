ffe16a70184886c4c61c000ae45335cf7f203d0d
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-02-22 21:30:21 +0000

    Basic Build complete

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div  class="bg-gray-900  text-gray-100">
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <structure-header />
+    <slot />
+    <structure-footer />
+  </div>
+</template>
+
+<style scoped></style>
