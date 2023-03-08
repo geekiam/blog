30feb4212223c9ccc26db2f0c457dd169f3e0e79
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-03-08 21:23:46 +0000

    added post header

---
+++ components/post/header.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import {PropType} from "@vue/runtime-core";
+import {PostHeading} from "~/types/PostHeading";
+
+const props = defineProps({
+  post: {
+    type: Object as PropType<PostHeading>
+  }
+
+})
+</script>
+
+<template>
+  <div class="p-0">
+    <h1>{{post.title}}</h1>
+  </div>
+</template>
+
+<style scoped></style>
