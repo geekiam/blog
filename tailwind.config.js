6546b0b37e7f87e6f8f887b7a31823bdf1d15ae1
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-05-18 22:16:02 +0100

    added images

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
-   require('@tailwindcss/aspect-ratio'),
-   require( '@tailwindcss/forms')
+    require('@tailwindcss/aspect-ratio'),
+    require( '@tailwindcss/forms')
   ],
 }
