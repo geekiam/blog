7613d87b48aca96fae9dfba9a6bf5ef7470e7fba
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-05-19 22:28:22 +0100

    Added Fantom Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
         },
         runtimeConfig: {
             public: {
-                google_analytics_id: process.env.fantom_analytics_id,
+                fantom_analytics_id: process.env.fantom_analytics_id,
                 production_mode: isProduction
 
             }
--- plugins/fantom.client.ts
+++ plugins/fantom.client.ts
@@ -4,7 +4,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     const config = useRuntimeConfig()
     if(config.public.production_mode) {
         nuxtApp.vueApp.use(VueFathom, {
-            siteID: "YourSiteID",
+            siteID: config.public.fantom_analytics_id,
             settings: {
                 url: "geekiam.io",
                 spa: "history",
