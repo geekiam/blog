50f95df50a58711fa14f214d0297cecf1a40616b
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-05-20 00:02:51 +0100

    Added Fantom Analytics

--- plugins/fantom.client.ts
+++ plugins/fantom.client.ts
@@ -2,7 +2,7 @@ import { defineNuxtPlugin } from "#app";
 import VueFathom from "@ubclaunchpad/vue-fathom";
 export default defineNuxtPlugin((nuxtApp) => {
     const config = useRuntimeConfig()
-    if(config.public.production_mode) {
+
         nuxtApp.vueApp.use(VueFathom, {
             siteID: config.public.fantom_analytics_id,
             settings: {
@@ -10,5 +10,5 @@ export default defineNuxtPlugin((nuxtApp) => {
                 spa: "history",
             },
         });
-    }
+
 });
