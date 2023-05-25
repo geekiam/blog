81b8f722f47a8ba9cc29168079b30b8abcc6cc1d
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-05-25 21:37:26 +0100

    Added SEO Meta to posts

--- app.vue
+++ app.vue
@@ -1,3 +1,34 @@
+<script lang="ts">
+useHead({
+  title: 'Geekiam',
+  meta: [
+    {
+      name: 'description',
+      content: 'Discover, Organise, Share and Promote self sovereignty',
+    },
+    { name: 'og:title', content: 'Geekiam' },
+    {
+      name: 'og:description',
+      content: 'Discover, Organise, Share and Promote self sovereignty',
+    },
+    { name: 'og:image', content: 'https://geekiam.io/geekiam.png' },
+    { name: 'og:url', content: 'https://geekiam.io' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:site', content: '@geekiamio' },
+    { name: 'twitter:creator', content: '@geekiamio' },
+    { name: 'twitter:title', content: 'Geekiam' },
+    {
+      name: 'twitter:description',
+      content: 'Discover, Organise, Share and Promote learning in communities of interest',
+    },
+    { name: 'twitter:image', content: 'https://geekiam.io/geekiam.png' },
+  ],
+  script: [
+    { src:'' },
+  ]
+})
+
+</script>
 <template>
   <div  class="bg-gray-200 dark:bg-gray-800 dark:text-gray-100 text-gray-800">
     <NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            charset: 'utf-8',
-            meta: [
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-            ],
-            script: [
-                { src: ''},
-                ]
-        }
-    },
-    content: {
+        content: {
         highlight: {
 
             theme: {
