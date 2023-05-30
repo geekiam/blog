8678c8c17bc9928c62f3e6b0b9286c4026fa85a0
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-05-30 09:52:14 +0100

    Slight layout changes

--- components/post/header.vue
+++ components/post/header.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 
 <template>
   <div class="w-full mx-auto pt-5 pb-2 mt-3 mb-5 shadow-2xl">
-    <div class="mt-3 text-center">
+    <div class="mt-3 text-center sm:p-10 p-2">
       <h1 class="text-orange-500 text-center text-6xl mb-3">{{ post.title }}</h1>
       <p class="dark:text-gray-200 text-gray-800 text-center text-2xl mt-2 mb-2">{{ post.subtitle }}</p>
       <p class="text-md font-semibold italic  dark:text-gray-300 text-gray-800 text-justify mb-3">
@@ -20,15 +20,15 @@ const props = defineProps({
       </p>
       <div class="overflow-hidden">
         <div class="">
-          <div class="sm:flex sm:items-center sm:justify-between">
+          <div class="sm:flex items-center sm:justify-between">
             <div class="sm:flex sm:space-x-5">
               <div class="flex-shrink-0">
-                <img class="mx-auto h-12 w-12 rounded-full" :src="post?.author?.imageUrl" :alt="post?.author?.name"
+                <img class="mx-auto sm:h-12 sm:w-12 h-6 w-6  rounded-full" :src="post?.author?.imageUrl" :alt="post?.author?.name"
                      :title="post?.author?.name"/>
               </div>
               <div class="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
-                <p class="text-orange-500 text-lg font-bold sm:text-xl">{{ post?.author?.name }}</p>
-                <p class="text-sm text-gray-200">
+                <p class="text-orange-500 font-bold sm:text-sm">{{ post?.author?.name }}</p>
+                <p class="text-sm sm:text-xs  text-gray-800 dark:text-gray-200">
                   <span>{{post?.publishDate}}</span>
                 </p>
               </div>
